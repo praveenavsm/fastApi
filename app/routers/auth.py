98e16550082d20cac0b86294256c6fe0e5a20ac3
Abhi <10001013@mcpsmd.net>
2022-03-16 11:15:21 -0400

    Set up access token logic

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from fastapi import status, HTTPException, Depends, APIRouter
 
-from app import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
@@ -17,6 +17,7 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
 
     # create token
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
 
     # return tokens
-    return {"token": "Example token"}
+    return {"access_token": access_token, "token_type": "bearer"}
